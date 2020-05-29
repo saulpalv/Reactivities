21e9d669d5b35e6da421c3ca7681240cd01e737a
saulpalv <saulalonsopalazuelos@outlook.com>
2020-05-29 17:49:08 -0600

    5. reactivities fetch class component

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Header, Icon, List } from 'semantic-ui-react';
+import axios from 'axios';
+import { IActivity } from '../models/activity';
+// 
+
+interface IState {
+  activities: IActivity[]
+}
+
+class App extends Component<{}, IState> {
+
+  readonly state: IState = {
+    activities: []
+  }
+
+  componentDidMount() {
+    axios.get<IActivity[]>('http://localhost:5000/api/activities')
+      .then((response) => {
+        this.setState({
+          activities: response.data
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header as='h2'>
+          <Icon name='users' />
+          <Header.Content>Reactivities</Header.Content>
+        </Header>
+        <List>
+
+          {this.state.activities.map((activity) => (
+            <List.Item key={activity.id}>{activity.title}</List.Item>
+          ))}
+
+        </List>
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client-app/src/app/models/activity.ts
@@ -0,0 +1,9 @@
+export interface IActivity {
+    id: string;
+    title: string;
+    description: string;
+    categoty: string;
+    date: string;
+    city: string;
+    venue: string;
+}
